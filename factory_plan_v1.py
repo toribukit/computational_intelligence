e58591e1dc2ed8e0834b1313ab77512537c46ea3
toribukit <toribukit@gmail.com>
2023-06-16 01:00:05 +0900

    run evolution

--- factory_plan_v1.py
+++ factory_plan_v1.py
@@ -6,6 +6,8 @@ from collections import namedtuple
 import random
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+# Set the random seed
+# random.seed(420)
 
 Genome = List[int]
 Population = List[Genome]
@@ -15,16 +17,17 @@ SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Genome, Genome]]
 CrossoverFunct = Callable[[Genome, Genome], Tuple[Genome, Genome]]
 MutationFunct = Callable[[Genome], Genome]
 
-POPULATION_SIZE = 3
+POPULATION_SIZE = 10
 NUM_PLANTS = 3
 NUM_FIELDS = 3
 PRODUCTION_QTY = 100
-ADDITIONAL_COST_PER_FIELD = 100
+ADDITIONAL_COST_PER_FIELD = 50
 
 Plant_capacity = [int(PRODUCTION_QTY/4) for _ in range(NUM_PLANTS*NUM_FIELDS)]
 plant_cost = [50, 30, 20]
 plant_qty = [0.9, 0.8, 0.5]
-field_cost = [11.7, 48.8, 36.5, 22.1, 10.4, 21.5, 11.5, 10.3, 19.1]
+# field_cost = [11.7, 48.8, 36.5, 22.1, 10.4, 21.5, 11.5, 10.3, 19.1]
+field_cost = [61.7, 48.8, 36.5, 22.1, 10.4, 21.5, 11.5, 10.3, 19.1]
 # field_cost = [((plant_cost[int(i/NUM_FIELDS)])*random.random()) for i in range(NUM_PLANTS*NUM_FIELDS)]
 # data = np.array(field_cost).reshape(-1, 1)
 # scaler = MinMaxScaler()
@@ -97,10 +100,11 @@ def generate_population(size: int, num_plants: int, prod_qty: int) -> Population
     return [generate_genome(num_plants, prod_qty) for _ in range(size)]
 
 population_gen = generate_population(POPULATION_SIZE, NUM_PLANTS*NUM_FIELDS, PRODUCTION_QTY)
-print(population_gen)
-for i in range(POPULATION_SIZE):
-    dec_res = decode(NUM_PLANTS*NUM_FIELDS, population_gen[i], PRODUCTION_QTY)
-    print(f'decode: {dec_res} ; SUM {sum(dec_res)}')
+
+# print(population_gen)
+# for i in range(POPULATION_SIZE):
+#     dec_res = decode(NUM_PLANTS*NUM_FIELDS, population_gen[i], PRODUCTION_QTY)
+#     print(f'decode: {dec_res} ; SUM {sum(dec_res)}')
 
 def fitness(genome: Genome, cost_f: [float], qty_f: [float], field_per_plant: int, num_plant:int, total_qty: int, cost_p: [int]) -> float:
     total_field = field_per_plant*num_plant
@@ -180,6 +184,11 @@ def uniform_crossover(a: Genome, b: Genome) -> Tuple[Genome, Genome]:
 def perform_correction(offspring:Genome, field_per_plant: int, num_plant:int, total_qty: int, capacity: [int]) -> Genome:
     total_field = field_per_plant * num_plant
     list_prod = decode(total_field, offspring, total_qty)
+
+    for i in range(len(list_prod)):
+        if list_prod[i] > capacity[i]:
+            list_prod[i] = capacity[i]
+
     diff = total_qty - (sum(list_prod))
     # print(f'before correction: {list_prod}')
 
@@ -200,19 +209,19 @@ def perform_correction(offspring:Genome, field_per_plant: int, num_plant:int, to
 
     return offspring
 
-fitness_func=partial(
-        fitness, cost_f=field_cost, qty_f=field_qty, field_per_plant=NUM_FIELDS, num_plant=NUM_PLANTS, total_qty=PRODUCTION_QTY, cost_p=plant_cost
-    )
-parents = selection_pair(population_gen, fitness_func)
-offspring_a, offspring_b = uniform_crossover(parents[0], parents[1])
-dec_res = decode(NUM_PLANTS*NUM_FIELDS, offspring_a, PRODUCTION_QTY)
-print(f'decode offspring 1: {dec_res} ; SUM {sum(dec_res)}')
-dec_res = decode(NUM_PLANTS*NUM_FIELDS, offspring_b, PRODUCTION_QTY)
-print(f'decode offspring 2: {dec_res} ; SUM {sum(dec_res)}')
-dec_res = decode(NUM_PLANTS*NUM_FIELDS, parents[0], PRODUCTION_QTY)
-print(f'decode parent 1: {dec_res} ; SUM {sum(dec_res)}')
-dec_res = decode(NUM_PLANTS*NUM_FIELDS, parents[1], PRODUCTION_QTY)
-print(f'decode parent 2: {dec_res} ; SUM {sum(dec_res)}')
+# fitness_func=partial(
+#         fitness, cost_f=field_cost, qty_f=field_qty, field_per_plant=NUM_FIELDS, num_plant=NUM_PLANTS, total_qty=PRODUCTION_QTY, cost_p=plant_cost
+#     )
+# parents = selection_pair(population_gen, fitness_func)
+# offspring_a, offspring_b = uniform_crossover(parents[0], parents[1])
+# dec_res = decode(NUM_PLANTS*NUM_FIELDS, offspring_a, PRODUCTION_QTY)
+# print(f'decode offspring 1: {dec_res} ; SUM {sum(dec_res)}')
+# dec_res = decode(NUM_PLANTS*NUM_FIELDS, offspring_b, PRODUCTION_QTY)
+# print(f'decode offspring 2: {dec_res} ; SUM {sum(dec_res)}')
+# dec_res = decode(NUM_PLANTS*NUM_FIELDS, parents[0], PRODUCTION_QTY)
+# print(f'decode parent 1: {dec_res} ; SUM {sum(dec_res)}')
+# dec_res = decode(NUM_PLANTS*NUM_FIELDS, parents[1], PRODUCTION_QTY)
+# print(f'decode parent 2: {dec_res} ; SUM {sum(dec_res)}')
 
 # offspring_a_correct = perform_correction(offspring_a, NUM_FIELDS, NUM_PLANTS, PRODUCTION_QTY, Plant_capacity)
 # dec_res = decode(NUM_PLANTS*NUM_FIELDS, offspring_a_correct, PRODUCTION_QTY)
@@ -227,27 +236,35 @@ print(f'decode parent 2: {dec_res} ; SUM {sum(dec_res)}')
 def mutation(genome: Genome, num: int = 1, probability: float= 0.5) -> Genome:
     for _ in range(num):
         index = randrange(len(genome))
-        genome[index] = genome[index] if random() > probability else abs(genome[index]-1)
+        # print(genome)
+        # print(genome[index])
+        genome[index] = genome[index] if random.random() > probability else abs(genome[index]-1)
     genome = perform_correction(genome, NUM_FIELDS, NUM_PLANTS, PRODUCTION_QTY, Plant_capacity)
 
     return genome
 
+
+# mutation_func = mutation
+# dec_res = decode(NUM_PLANTS*NUM_FIELDS, offspring_a, PRODUCTION_QTY)
+# print(f'decode offspring 1: {dec_res} ; SUM {sum(dec_res)}')
+# offspring_a = mutation_func(offspring_a)
+# dec_res = decode(NUM_PLANTS*NUM_FIELDS, offspring_a, PRODUCTION_QTY)
+# print(f'decode offspring 1: {dec_res} ; SUM {sum(dec_res)}')
+
 def run_evolution(
         populate_func: PopulateFunc,
         fitness_func: FitnessFunc,
-        fitness_limit: int,
         selection_func: SelectionFunc = selection_pair,
-        crossover_funct: CrossoverFunct = single_point_crossover,
+        crossover_funct: CrossoverFunct = uniform_crossover,
         mutation_func: MutationFunct = mutation,
         generation_limit: int = 100
 ) -> Tuple[Population, int]:
     population = populate_func()
 
     for i in range(generation_limit):
-        population = sorted(population, key=lambda genome: fitness_func(genome), reverse=True)
-
-        if fitness_func(population[0]) >= fitness_limit:
-            break
+        population = sorted(population, key=lambda genome: fitness_func(genome), reverse=False)
+        print(f"generations: {i}")
+        print(f"fitness: {fitness(population[0], field_cost, field_qty, NUM_FIELDS, NUM_PLANTS, PRODUCTION_QTY, plant_cost)}")
 
         next_generation = population[0:2]
 
@@ -263,31 +280,28 @@ def run_evolution(
     population = sorted(
         population,
         key=lambda genome: fitness_func(genome),
-        reverse=True
+        reverse=False
     )
+    print(f"generations: {i}")
+    print(f"fitness: {fitness(population[0], field_cost, field_qty, NUM_FIELDS, NUM_PLANTS, PRODUCTION_QTY, plant_cost)}")
+
 
     return population, i
 
 start = time.time()
 population, generation = run_evolution(
     populate_func=partial(
-        generate_population, size=10, genome_length=len(more_things)
+        generate_population, size=POPULATION_SIZE, num_plants=NUM_PLANTS*NUM_FIELDS, prod_qty=PRODUCTION_QTY
     ),
     fitness_func=partial(
-        fitness, things=more_things, weight_limit=3000
+        fitness, cost_f=field_cost, qty_f=field_qty, field_per_plant=NUM_FIELDS, num_plant=NUM_PLANTS, total_qty=PRODUCTION_QTY, cost_p=plant_cost
     ),
-    fitness_limit=1310,
     generation_limit=100
 )
 end = time.time()
 
-def genome_to_things(genome: Genome, things: [Thing]) -> [Thing]:
-    result = []
-    for i, thing in enumerate(things):
-        if genome[i] == 1:
-            result += [thing.name]
-    return result
-
 print(f"number of generations : {generation}")
 print(f"time: {end - start}s")
-print(f"best solution: {genome_to_things(population[0], more_things)}")
+dec_res = decode(NUM_PLANTS*NUM_FIELDS, population[0], PRODUCTION_QTY)
+print(f'Best Solutions: {dec_res} ; SUM {sum(dec_res)}')
+print(f"fitness stochastic: {fitness(population[0], field_cost, field_qty, NUM_FIELDS, NUM_PLANTS, PRODUCTION_QTY, plant_cost)}")
